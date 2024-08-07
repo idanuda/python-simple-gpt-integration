18e1073eb207914fbc7d46f2945441f565fee254
idanyehuda <90613271+idanyehuda@users.noreply.github.com>
2024-08-07 14:05:15 +0300

    Adding Services and Controllers

--- controllers/openai_controller.py
+++ controllers/openai_controller.py
@@ -1,24 +1,17 @@
 from flask import make_response, Blueprint, request
-from langchain_community.tools.tavily_search import TavilySearchResults
-from tavily import TavilyClient
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from pinecone import Pinecone
-
-
 import requests
 
 
 openai_controller = Blueprint('openai_controller', __name__, template_folder='templates')
-ENDPOINT = "YOUR-ENDPOINT"
-API_KEY = "YOU-API-KEY"
+ENDPOINT = ""
+API_KEY = ""
 
 @openai_controller.route('/api/v0/openai/gpt-4', methods=(['POST']))
 def gpt_chat():
-
     # Configuration
-
     headers = {
         "Content-Type": "application/json",
         "api-key": API_KEY,
@@ -59,9 +52,6 @@ def gpt_chat():
         "top_p": 0.95,
         "max_tokens": 800
     }
-
-
-
     # Send request
     try:
         response = requests.post(ENDPOINT, headers=headers, json=payload)
@@ -69,7 +59,6 @@ def gpt_chat():
     except requests.RequestException as e:
         print(e)
         raise SystemExit(f"Failed to make the request. Error: {e}")
-
 
     # Handle the response as needed (e.g., print or process)
     print(response.json())
